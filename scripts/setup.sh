690d2aa04fc2964f4133142c3cbe679c316fa2dd
Marc Berger <marc@fourkitchens.com>
2024-01-12 11:19:45 -0800

    feat: Prepare for generic setup of other sites

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+# Change origin of git repo
+read -p "What is the git URL for this new site? [git-url]: " git-url
+git_url=${git_url:-git-url}
+git remote set-url origin $git_url
+
+# Replace Lando site URL and name
+read -p "What is the machine name of your local site? [site-name]: " site-name
+site_name=${site_name:-site-name}
+sed -i '' "s/local-drupal-dev/$site_name/g" .lando.yml
+
+# Lando init
+lando rebuild -y
+
+#Composer install
+lando composer install
