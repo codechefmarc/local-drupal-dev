9ff23f6b86439d58d8c92aec54a4b3e9d527b18d
Marc Berger <marc@fourkitchens.com>
2024-01-12 11:24:45 -0800

    fix: Fix setup script reading variables

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 # Change origin of git repo
-read -p "What is the git URL for this new site? [git-url]: " git-url
+read -p "What is the git URL for this new site? [git-url]: " git_url
 git_url=${git_url:-git-url}
 git remote set-url origin $git_url
 
 # Replace Lando site URL and name
-read -p "What is the machine name of your local site? [site-name]: " site-name
+read -p "What is the machine name of your local site? [site-name]: " site_name
 site_name=${site_name:-site-name}
 sed -i '' "s/local-drupal-dev/$site_name/g" .lando.yml
 
