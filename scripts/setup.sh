9fe8ce160dd75b2882fdf06938bd3c3c9f20071d
Marc Berger <marc@fourkitchens.com>
2024-01-13 11:46:57 -0800

    feat: Make initial setup questions easier

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,12 @@
-# Change origin of git repo
-read -p "What is the git URL for this new site? [git-url]: " git_url
-git_url=${git_url:-git-url}
-git remote set-url origin $git_url
-
 # Replace Lando site URL and name
 read -p "What is the machine name of your local site? [site-name]: " site_name
 site_name=${site_name:-site-name}
 sed -i '' "s/local-drupal-dev/$site_name/g" .lando.yml
+
+# Change origin of git repo
+read -p "What is the git URL for this new site? [git@github.com:codechefmarc/$site_name.git]: " git_url
+git_url=${git_url:-"git@github.com:codechefmarc/$site_name.git"}
+git remote set-url origin $git_url
 
 # Lando init
 lando rebuild -y
