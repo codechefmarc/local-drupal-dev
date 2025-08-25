ba552086bbfc39ee65e0c2dd1db32ebbfd3b3562
Marc Berger <codechefmarc@gmail.com>
2025-08-25 11:59:11 -0700

    feat: Change setup script to install recipe

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,13 +12,13 @@ git remote set-url origin $git_url
 # Add original repo to be able to more easily get updates.
 git remote add original git@github.com:codechefmarc/local-drupal-dev.git
 
-# Lando init.
-lando rebuild -y
-lando start
+# Ddev init.
+ddev start
 
-#Composer and site pofile install.
-lando composer install
-lando drush si local_drupal_dev_profile --db-url=mysql://drupal10:drupal10@database:3306/drupal10 -y
+#Composer, install drupal and recipe.
+ddev composer install
+ddev drush site:install --account-name=admin --account-pass=admin -y
+ddev drush recipe ../recipes/local-drupal-dev
 
 # set site name.
 site_title="$(print -r -- ${(C)site_name})"
